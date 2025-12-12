cbd76106312f6d27671d289424894045ad19b9a9
Nicole Hao <nicolehao7@gmail.com>
2025-12-11 21:40:56 -0500

    Remove unused modules and scripts from numerics package

--- numerics/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .data import generate_dataset
-# from .model import FNO1D
-# from .train import train_fno
-# from .sobolev import H1_norm
--- numerics/sobolev.py
+++
@@ -1,9 +0,0 @@
-# H^norm error calculation
-
-import numpy as np
-
-def H1_norm(u, dx):
-    u = np.array(u)
-    du_dx = np.gradient(u, dx)
-    return np.sqrt(np.sum(u**2) * dx + np.sum(du_dx**2) * dx)
-
